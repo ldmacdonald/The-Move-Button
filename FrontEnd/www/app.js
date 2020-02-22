75bbbde62baa80484ee56182cedea7106e460741
karthik.ns <karthik.ns@fireeye.com>
2020-02-22 15:47:26 -0800

    jquery

---
+++ FrontEnd/www/app.js
@@ -0,0 +1,31 @@
+//$(document).ready(function() {
+//    showmenu();
+//});
+
+function showtimer(){
+    console.log("kns")
+    console.log($("#actions").find(":selected").text());
+    console.log($("#time_intervals").find(":selected").text());
+
+    var jqxhr = $.post( "login", function() {
+          alert( "success" );
+        })
+          .done(function() {
+            alert( "second success" );
+          })
+          .fail(function() {
+            alert( "error" );
+          })
+          .always(function() {
+            alert( "finished" );
+          });
+
+        // Perform other work here ...
+
+        // Set another completion function for the request above
+        jqxhr.always(function() {
+          alert( "second finished" );
+        });
+
+
+}

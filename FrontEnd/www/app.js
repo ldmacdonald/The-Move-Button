099a44b75cca5adb3eaa6f67df3bda824130da79
karthik.ns <karthik.ns@fireeye.com>
2020-02-22 17:07:31 -0800

    updated code

--- FrontEnd/www/app.js
+++ FrontEnd/www/app.js
@@ -1,15 +1,48 @@
-//$(document).ready(function() {
-//    showmenu();
-//});
+$(document).ready(function() {
+    $("#action_btn").hide()
+    $("#action_complete_btn").hide()
+});
 
-function showtimer(){
-    console.log("kns")
+function setschedule(){
     console.log($("#actions").find(":selected").text());
     console.log($("#time_intervals").find(":selected").text());
 
 
     var jqxhr = $.post( "sendEvent", function( data ) {
-       alert( "Data Loaded: " + data );
-     });
+        $("#set").hide()
+        $("#timeinterval_div").hide();
+        $("#actions_div").hide();
 
+        $("#action_btn").show();
+    });
+}
+
+function updatedb() {
+
+    var jqxhr = $.post( "sendEvent", function( data ) {
+
+            //var timer = now + Math.floor((now % (1000 * 60 * 60)) / (1000 * 60));
+//            $("#set").hide()
+//            $("#timeinterval_div").hide();
+//            $("#actions_div").hide();
+
+            //$("#action_btn").hide();
+            $("#action_btn").text("Start Timer");
+            if ($("#action_btn").text() == "Start Timer"){
+                console.log("timer started");
+                //timer();
+            }
+        });
+}
+
+function timer(){
+    console.log("timer started");
+//    var now = new Date().getTime();
+//    console.log(now);
+//    i = 0;
+//    while ( i < 600 ) {
+//        i++;
+//        //break;
+//    }
+    updatedb();
 }
